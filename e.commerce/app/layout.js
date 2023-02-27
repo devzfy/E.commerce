f43699943d9e956ea6ffcd5ecf92b4bfd5a4a5f6
javohir <73492437+exJavokhir@users.noreply.github.com>
2023-02-27 10:04:53 +0500

    new

---
+++ e.commerce/app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout ({children}){
+    return <html>
+        <head>
+            <title>E.commerce</title>
+        </head>
+        <body>
+            {children}
+        </body>
+    </html>
+}
